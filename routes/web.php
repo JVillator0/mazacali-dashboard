2eeb642ccc60d1ca176c6b0293e2f7e0bdf5b023
Javier Villatoro <javier99.villatoro@gmail.com>
2025-06-26 04:33:56 -0600

    ✨ Healt check endpoint

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect()->to('/admin');
 });
+
+
+Route::get('/health-check', function () {
+    return response()->json(['status' => 'ok']);
+});
